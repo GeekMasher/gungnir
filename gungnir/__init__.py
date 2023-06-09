63bca7421769f448aecded91cc1f8fe6d4decd04
GeekMasher <geekmasher@gmail.com>
2023-06-09 01:37:23 +0100

    feat: Add initial lib

---
+++ gungnir/__init__.py
@@ -0,0 +1,25 @@
+
+__name__ = "gungnir"
+__title__ = "Gungnir"
+
+__version__ = "0.1.0"
+
+__description__ = "Gungnir - Homelab Automation Bot to Guard your very own Asgard"
+__summary__ = "Homelab Automation Bot to Guard your very own Asgard"
+
+__url__ = "https://github.com/GeekMasher/gungnir"
+
+__license__ = "MIT License"
+__copyright__ = "Copyright (c) 2023, GeekMasher"
+
+__author__ = "GeekMasher"
+
+__banner__ = f"""\
+  ________                            .__        
+ /  _____/ __ __  ____    ____   ____ |__|______ 
+/   \\  ___|  |  \\/    \\  / ___\\ /    \\|  \\_  __ \\
+\\    \\_\\  \\  |  /   |  \\/ /_/  >   |  \\  ||  | \\/
+ \\______  /____/|___|  /\\___  /|___|  /__||__|   
+        \\/           \\//_____/      \\/        v{__version__}
+"""
+
