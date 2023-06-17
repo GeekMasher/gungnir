1f97541821a9c296f525458326ca40d98a6fa5e9
GeekMasher <geekmasher@gmail.com>
2023-06-17 19:53:40 +0100

    feat: Add Entrypoint for containers

---
+++ entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+if [[ $1 == "setup" ]]; then
+    echo "Setting up cron..."
+    CRON=${CRON:=15 * * * *}
+    echo "Cron used :: '${CRON}'"
+
+    echo "${CRON} /app/entrypoint.sh run" >> /etc/crontabs/gungnir
+    chmod 0644 /etc/crontabs/gungnir
+
+    crontab /etc/crontabs/gungnir
+    touch /var/log/cron.log
+
+    echo "Setup complete!"
+
+elif [[ $1 == "run" ]]; then
+    echo "Running workload..."
+    export PYTHONPATH=/app:/app/gungnir
+    python -m gungnir --container
+
+    exit 0
+fi
+
+echo "Waiting for cron to trigger..."
+/usr/sbin/crond -f -l 8
+
